affcb05d56e8776e0c9b64d8aacf6c528fc19324
GUSTAVO OLIVEIRA <gugutoktpi2@gmail.com>
2025-09-18 22:22:23 -0300

    Arquivos utilizados ou alterados na aula

---
+++ aulaTypeScript/funcoes.ts
@@ -0,0 +1,28 @@
+//Função que retorna tipos 
+function saudacao(nome: string): string{
+    return `Olá, ${nome}!`
+}
+
+console.log(saudacao('Leandro'));
+
+//Interface para objeto Usuario
+interface Usuario {
+    nome: string;
+    idade: number;
+    email?: string; //opcional
+}
+
+//Utilizando a interface usuário fica assim:
+function exibirUsuario(usuario: Usuario):void{
+    console.log(`nome: ${usuario.nome}`);
+    console.log(`idade: ${usuario.idade}`);
+}
+
+exibirUsuario({ nome: 'Marcos', idade: 22})
+
+//Exemplo de uma função que retorna arrays e tem parametros opcionais
+function listarNomes(nomes:string[]): void {
+    nomes.forEach(nome => console.log(nome));
+}
+
+listarNomes(['Ana', 'Bruno', 'Carlos']);
